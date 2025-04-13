680a12a574c75e6e21a39bda9815d8b651e138b9
belkius <belkius102@gmail.com>
2025-04-14 00:56:24 +0600

    Implement user registration and authentication with JWT support

--- src/main/java/io/paws/paws/controller/UserController.java
+++ src/main/java/io/paws/paws/controller/UserController.java
@@ -2,6 +2,7 @@ package io.paws.paws.controller;
 
 import io.paws.paws.DTO.SigninRequest;
 import io.paws.paws.DTO.SignupRequest;
+import io.paws.paws.DTO.UserResponse;
 import io.paws.paws.repository.UserRepository;
 import io.paws.paws.entity.User;
 import io.paws.paws.repository.PetsRepository;
@@ -24,8 +25,58 @@ public class UserController {
 
     @Autowired
     UserRepository userRepository;
+
     @Autowired
     private JwtService jwtService;
+
+    @GetMapping("/get-me")
+    public ResponseEntity<?> getMe(@RequestHeader("Authorization") String token) {
+        try {
+            // Убираем "Bearer " из строки токена
+            String jwtToken = token.substring(7);
+            // Получаем email из токена
+            String email = jwtService.extractEmail(jwtToken);
+            // Находим пользователя по email
+            User user = userRepository.findByEmail(email);
+
+            if (user == null) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Пользователь не найден");
+            }
+            // Отправляем данные пользователя
+            // Вернем данные только те поля, которые хотим показать во фронтенде (name, email, tel, token)
+            return ResponseEntity.ok(new UserResponse(user.getName(), user.getEmail(), user.getTel(), user.getToken()));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Неверный или истекший токен");
+        }
+    }
+
+    @PostMapping("/set-me")
+    public ResponseEntity<?> setMe(@RequestHeader("Authorization") String token, @RequestBody Map<String, String> updates) {
+        try {
+            // Убираем "Bearer " из строки токена
+            String jwtToken = token.substring(7);
+            // Получаем email из токена
+            String email = jwtService.extractEmail(jwtToken);
+            // Находим пользователя по email
+            User user = userRepository.findByEmail(email);
+            if (user == null) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Пользователь не найден");
+            }
+            // Обновляем данные пользователя
+            if (updates.containsKey("name")) {
+                user.setName(updates.get("name"));
+            }
+            if (updates.containsKey("tel")) {
+                user.setTel(Integer.parseInt(updates.get("tel")));
+            }
+            // Сохраняем обновленные данные
+            userRepository.save(user);
+
+            return ResponseEntity.ok("Данные пользователя обновлены");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Неверный или истекший токен");
+        }
+    }
 
     @PostMapping("/signup")
     public ResponseEntity<?> signup(@RequestBody @Valid SignupRequest request) {
